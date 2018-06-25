70d99a1aa74d7d67b0620baae7d7355aa27ff93e
ondele <mr.tchipepe@outlook.com>
2018-06-25 22:50:38 +0100

    First commit to git hub

---
+++ Unico/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Unico.Startup))]
+namespace Unico
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
